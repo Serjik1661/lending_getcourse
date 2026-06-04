5a58cb99cbda854e7d3e10a5d8c41103ab0e0451
Sergey Dashtamirov <cosmostar2013@rambler.ru>
2026-06-04 21:12:20 +0300

    Tighten hero badge spacing

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,7 +3,7 @@
 
   if (!("serviceWorker" in navigator)) return;
 
-  const SW_VERSION = "20260604-17";
+  const SW_VERSION = "20260604-18";
 
   window.addEventListener("load", () => {
     navigator.serviceWorker.register(`./sw.js?v=${SW_VERSION}`, {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "propolis-static-v18";
+const CACHE_NAME = "propolis-static-v19";
 const LOCAL_ASSETS = [
   "./",
   "404.html",
