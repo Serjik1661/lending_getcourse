c125031f7ee4adea4258107addf8e99925e2bae5
Sergey Dashtamirov <cosmostar2013@rambler.ru>
2026-06-04 21:18:36 +0300

    Force two-line hero tags

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,7 +3,7 @@
 
   if (!("serviceWorker" in navigator)) return;
 
-  const SW_VERSION = "20260604-18";
+  const SW_VERSION = "20260604-19";
 
   window.addEventListener("load", () => {
     navigator.serviceWorker.register(`./sw.js?v=${SW_VERSION}`, {
