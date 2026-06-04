81a4e6008ed44796ac35752f5e6f83ae4c9645ff
Sergey Dashtamirov <cosmostar2013@rambler.ru>
2026-06-04 20:24:35 +0300

    Refresh cache and mobile hero

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,7 +3,7 @@
 
   if (!("serviceWorker" in navigator)) return;
 
-  const SW_VERSION = "20260604-5";
+  const SW_VERSION = "20260604-6";
 
   window.addEventListener("load", () => {
     navigator.serviceWorker.register(`./sw.js?v=${SW_VERSION}`, {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "propolis-static-v6";
+const CACHE_NAME = "propolis-static-v7";
 const LOCAL_ASSETS = [
   "./",
   "404.html",
