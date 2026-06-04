29b5f19604d5e95ce2102438a53dd374c3a733a3
Sergey Dashtamirov <cosmostar2013@rambler.ru>
2026-06-04 20:11:30 +0300

    Unify footer and mobile hero layout

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,7 +3,7 @@
 
   if (!("serviceWorker" in navigator)) return;
 
-  const SW_VERSION = "20260604-3";
+  const SW_VERSION = "20260604-4";
 
   window.addEventListener("load", () => {
     navigator.serviceWorker.register(`./sw.js?v=${SW_VERSION}`, {
