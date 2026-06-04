922cac61da26b9b2bd0eb0ae51431905027a4dfb
Sergey Dashtamirov <cosmostar2013@rambler.ru>
2026-06-04 20:31:56 +0300

    Fix hero badge wrapping

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,7 +3,7 @@
 
   if (!("serviceWorker" in navigator)) return;
 
-  const SW_VERSION = "20260604-6";
+  const SW_VERSION = "20260604-7";
 
   window.addEventListener("load", () => {
     navigator.serviceWorker.register(`./sw.js?v=${SW_VERSION}`, {
